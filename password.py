238064e60f1c22c83af650b8607d645651e1083a
leonkae <leon29kae@gmail.com>
2022-04-23 08:32:05 +0300

    update on file

--- password.py
+++ password.py
@@ -21,12 +21,16 @@ class User:
     def save_user (self):
         User.user_list.append(self)
         
+        '''method that checks for string data type'''
     @classmethod
     def f_name_exists(cls,f_name):
         for user in cls.user_list:
             if user.f_name == {string}:
                 return True
             return print ("does not accept numbers/special character")    
+        
+        
+        '''method that checks for string data type'''
     
     @classmethod
     def l_name_exists(cls,l_name):
@@ -35,6 +39,7 @@ class User:
                 return True
             return print ("does not accept numbers/special character")
               
+        '''method that checks for string data type'''
         
     @classmethod
     def user_name_exists(cls,u_name):
@@ -43,11 +48,16 @@ class User:
                 return True
             return print("does not accept numbers/special characters")
        
+       
+        '''email check'''
+
     @classmethod
     def copy_email(cls,email):
         email_found = User.find_by_email(email)
         pyperclip.copy(email_found)
-    
+        
+        
+        '''password logic'''
     @classmethod
     def enter_password(cls,password):
         password   
