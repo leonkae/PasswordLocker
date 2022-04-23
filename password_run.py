3ed5dafdf73623e847727fd0922e58e0877edd98
leonkae <leon29kae@gmail.com>
2022-04-23 08:33:30 +0300

    update o run file

--- password_run.py
+++ password_run.py
@@ -15,14 +15,31 @@ def find_email(email):
     return User.find_by_email(email)
 
 def main():
-    print("welcome, This to PassWordLOcker")
+    print("welcome, To PassWordLOcker enter password to continue")
+    
+    '''login logic'''
+    
+    password = input()
+    if password == True:
+      return print ("enter User name to continue")
+    else: 
+        print ("wrong password")
     user_name = input()
-    print(f"welcome {user_name}, lets continue")
+    if user_name == True:
+        return print(f"welcome {user_name}, lets continue")
+    else:
+        print ("wrong user")
+    
+    
+    '''Creation of new account'''
+    # print(f"welcome {user_name}, lets continue")
     print('\n')
     while True:
         print("This short codes: cu-createuser, fm-findmail, ex-exit on the appliction")
         short_code = input().lower()
         if short_code == 'cu':
+            print("No account no problem")
+            
             print("New User")
             print("First Name")
             f_name = input()
@@ -39,6 +56,18 @@ def main():
             print ("password")
             password = input()
             
+            save_user(create_user(f_name,l_name,u_name,email,password))
+            print('\n')
+            print(f'Account {f_name}{l_name}{u_name}{email}{password} now exists')
+            
+            
+        elif short_code == 'ex':
+            print("come back soon dont be a stranger")
+            break 
+        else:
+            print("Invalid, Please use shortcodes")  
+            
+             
             
 if __name__ == '__main__':
     main()
