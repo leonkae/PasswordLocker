ca1a6088759f1d0b1ebbe7c1367a6b3579e2a7f4
leonkae <leon29kae@gmail.com>
2022-04-24 10:17:57 +0300

    updated//upgraded password

--- password.py
+++ password.py
@@ -1,5 +1,8 @@
+from multiprocessing.dummy.connection import families
 import pyperclip
 import string
+
+from sqlalchemy import false
 
 
 class User:
@@ -12,55 +15,105 @@ class User:
         
         self.f_name = f_name
         self.l_name = l_name
-        self.user_name = u_name
+        self.u_name = u_name
         self.email = email
         self.password = password
         
         ''' creating user account'''
-        
+    # @classmethod    
     def save_user (self):
-        User.user_list.append(self)
-        
-        '''method that checks for string data type'''
-    @classmethod
-    def f_name_exists(cls,f_name):
+        # check_user == user_exists
+        has_account = self.check_user(self.email)
+        if has_account:
+            # print("user exists,not saved")
+            return False
+        self.user_list.append(self)
+        return True
+            
+       
+    @classmethod    
+    def check_user(cls,email):
         for user in cls.user_list:
-            if user.f_name == {string}:
+            if user.email == email:
                 return True
-            return print ("does not accept numbers/special character")    
-        
-        
-        '''method that checks for string data type'''
+        return False
     
-    @classmethod
-    def l_name_exists(cls,l_name):
+    
+    @classmethod    
+    def user_login(cls,email,password):
         for user in cls.user_list:
-            if user.l_name == {string}:
+            if user.email == email and user.password == password: 
+                print("logged in Succesfully") 
                 return True
-            return print ("does not accept numbers/special character")
-              
+        print ("wrong email or password, please try again.")
+        return False    
+            
+                
+                
+            
+        # if new_account:
+        #     print ("set up account")
+        #     return True
+        # return False
+    
+         # @classmethod
+         # def check_account (cls, account):
+         #     if account:
+         #         return True
+         #     return False
+            
+            
+        
+        
+
+        
         '''method that checks for string data type'''
         
-    @classmethod
-    def user_name_exists(cls,u_name):
-        for user in cls.user_list:
-            if user.u_name == {string}:
-                return True
-            return print("does not accept numbers/special characters")
+    # @classmethod
+    # def f_name_exists(cls,f_name):
+    #     for user in cls.user_list:
+    #         if user.f_name == {string}:
+    #             return True
+    #         return print ("does not accept numbers/special character")    
+        
+        
+        
+    #     '''method that checks for string data type'''
+    
+    # @classmethod
+    # def l_name_exists(cls,l_name):
+    #     for user in cls.user_list:
+    #         if user.l_name == {string}:
+    #             return True
+    #         return print ("does not accept numbers/special character")
+              
+    #     '''method that checks for string data type'''
+        
+    # @classmethod
+    # def user_name_exists(cls,u_name):
+    #     for user in cls.user_list:
+    #         if user.u_name == {string}:
+    #             return True
+    #         return print("does not accept numbers/special characters")
        
        
-        '''email check'''
+    #     '''email check'''
 
-    @classmethod
-    def copy_email(cls,email):
-        email_found = User.find_by_email(email)
-        pyperclip.copy(email_found)
+    # @classmethod
+    # def lookthrough_email(cls,email):
+    #     email_found = User.find_by_email(email)
+    #     pyperclip.copy(email_found)
         
+    # @classmethod
+    # def find_by_email(cls,email):
+    #     pass 
+
         
-        '''password logic'''
-    @classmethod
-    def enter_password(cls,password):
-        password   
+    #     '''password logic'''
+        
+    # @classmethod
+    # def enter_password(cls,password):
+    #     password   
      
       
       
